3fb164654a762ed995969cf5f0e92520ea305a4b
Json-Liu <756289901@qq.com>
2016-09-03 18:27:46 +0800

    update

--- src/main/java/com/joson/HelloTest.java
+++ src/main/java/com/joson/HelloTest.java
@@ -1,14 +1,10 @@
 package com.joson;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HelloTest {
 
     public static void main(String[] args) {
-       /* RegexHelper regexHelper = new RegexHelper();
-        String number = "756289901@123.com";
-        System.out.println(regexHelper.isEmail(number));*/
         String regex = "[0-1][0-9]:[0-2][0-9]:[0-5][0-9]";
         try{
             System.out.println(Pattern.matches(regex, ""));
